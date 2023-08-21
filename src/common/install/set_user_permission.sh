66e2f45564dfd0019427695360127bba734e2155
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-08-21 12:46:38 +0100

    Update set_user_permission.sh

--- src/common/install/set_user_permission.sh
+++ src/common/install/set_user_permission.sh
@@ -13,9 +13,17 @@ do
     chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
 done
 echo "-------------------------------INSTALL DROID------------------------"
-echo "y" | /opt/android-sdk/cmdline-tools/tools/bin/sdkmanager "platforms;android-30" "system-images;android-30;google_apis_playstore;x86_64" "build-tools;30.0.2"
+echo "export ANDROID_SDK_ROOT=/opt/android-sdk" >> ~/.bashrc
+echo "export ANDROID_HOME=/opt/android-sdk" >> ~/.bashrc
+echo "export ANDROID_EMULATOR_WAIT_TIME_BEFORE_KILL=60" >> ~/.bashrc
+echo "export ANDROID_AVD_HOME=/root/.android/avd/" >> ~/.bashrc
+source ~/.bashrc
+
+/opt/android-sdk/cmdline-tools/tools/bin/sdkmanager "platforms;android-24" "system-images;android-24;default;x86_64" "build-tools;24.0.1"
+#echo "y" | /opt/android-sdk/cmdline-tools/tools/bin/sdkmanager "platforms;android-30" "system-images;android-30;google_apis_playstore;x86_64" "build-tools;30.0.2"
 echo "y" | /opt/android-sdk/cmdline-tools/tools/bin/sdkmanager platform-tools emulator
-#echo "no" | /opt/android-sdk/cmdline-tools/tools/bin/avdmanager create avd -n "my_avd_30" -k "system-images;android-30;google_apis_playstore;x86_64"
+echo "no" | /opt/android-sdk/cmdline-tools/tools/bin/avdmanager create avd -n "my_avd_24" -k "system-images;android-24;default;x86_64"
+
 #cd
 #wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
 #unzip commandlinetools-linux-10406996_latest.zip
