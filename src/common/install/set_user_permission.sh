dc12e1ef67b04e2d1a0804c138eda331053310a7
cata0nana <101589752+cata0nana@users.noreply.github.com>
2023-08-21 08:25:28 +0100

    Update set_user_permission.sh

--- src/common/install/set_user_permission.sh
+++ src/common/install/set_user_permission.sh
@@ -13,6 +13,9 @@ do
     chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
 done
 echo "-------------------------------INSTALL DROID------------------------"
+/opt/android-sdk/cmdline-tools/tools/bin/sdkmanager "platforms;android-30" "system-images;android-30;google_apis_playstore;x86_64" "build-tools;30.0.2"
+/opt/android-sdk/cmdline-tools/tools/bin/sdkmanager platform-tools emulator
+/opt/android-sdk/cmdline-tools/tools/bin/avdmanager create avd -n "my_avd_30" -k "system-images;android-30;google_apis_playstore;x86_64"
 #cd
 #wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
 #unzip commandlinetools-linux-10406996_latest.zip
